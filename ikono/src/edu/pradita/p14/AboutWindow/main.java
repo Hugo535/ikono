4398ef350e8d6b860a046f35609c4cefd994ee8c
Hugo535 <hugo.alextanoto@gmail.com>
2024-12-20 14:16:55 +0700

    Create main.java

---
+++ ikono/src/edu/pradita/p14/AboutWindow/main.java
@@ -0,0 +1,20 @@
+package edu.pradita.p10;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("about.fxml"));
+        primaryStage.setTitle("About Application");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
